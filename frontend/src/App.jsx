3ecd667250343cc8b5894a8c9aa82fda3831c471
zx18522296069-commits <zx18522296069@gmail.com>
2026-09-10 21:38:41 +0800

    add frontend main page

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>生产自动化控制台</h1>
+      <button>画图</button>
+      <button>拆图</button>
+      <button>未加工更新</button>
+    </div>
+  );
+}
+
+export default App;
