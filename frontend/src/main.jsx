22ede8493b8b1b8f68f514c237d9138de2f2c342
zx18522296069-commits <zx18522296069@gmail.com>
2026-09-16 20:39:55 +0800

    加载未加工结果分组样式

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,5 +2,6 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
 import './style.css';
+import './parts-result.css';
 
 createRoot(document.getElementById('root')).render(<App />);
